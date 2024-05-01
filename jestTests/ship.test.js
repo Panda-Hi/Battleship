13f4e89d4d06508ba776f586b5cc896d8dfa6188
Panda-Hi <118573854+Panda-Hi@users.noreply.github.com>
2024-05-01 16:01:33 +0200

    test: add jest test for ship factory function

--- jestTests/ship.test.js
+++ jestTests/ship.test.js
@@ -1,6 +1,37 @@
 import { ship } from "../src/ship";
 
-it("one", () => {
-  const val = ship();
-  expect(val).toBe(1);
+describe("test ship factory function", () => {
+  let testShip;
+  beforeEach(() => {
+    testShip = ship(4);
+  });
+  it("getShipLength should return declared ship length", () => {
+    expect(testShip.getShipLength()).toBe(4);
+  });
+  it("hit() must increase hitNumber by 1", () => {
+    testShip.hit();
+    expect(testShip.getHitNumber()).toBe(1);
+  });
+  it("hit() should not increase when hit number === length", () => {
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.getHitNumber()).toBe(4);
+  });
+
+  it("isSunk() should return true", () => {
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.isSunk()).toBe(true);
+  });
+  it("isSunk() should return false", () => {
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.isSunk()).toBe(false);
+  });
 });

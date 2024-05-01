7221ca12b31d8c725ce080aa69e6d5cc0e1f3cef
Panda-Hi <118573854+Panda-Hi@users.noreply.github.com>
2024-05-01 16:02:20 +0200

    feat: create ship factory function

--- src/ship.js
+++ src/ship.js
@@ -1,4 +1,19 @@
 /* eslint-disable import/prefer-default-export */
-console.log("js");
-
-// configure jest to accept es6 modules
+export function ship(length) {
+  const shipLength = length;
+  let hitNumber = 0;
+  const hit = () => {
+    if (hitNumber < shipLength) {
+      hitNumber += 1;
+    }
+  };
+  const isSunk = () => hitNumber === shipLength;
+  const getHitNumber = () => hitNumber;
+  const getShipLength = () => shipLength;
+  return {
+    hit,
+    isSunk,
+    getHitNumber,
+    getShipLength,
+  };
+}

dda2d33e7ec9dc76ec40160839c4ad593c2cc72f
Panda-Hi <118573854+Panda-Hi@users.noreply.github.com>
2024-05-18 13:30:30 +0200

    test: create receiveAttack + areAllShipsSunk tests

--- jestTests/gameboard.test.js
+++ jestTests/gameboard.test.js
@@ -1,6 +1,6 @@
 import { gameboard } from "../src/gameboard";
 // npm test gameboard.test.js
-describe("test gameboard placeShip()", () => {
+describe("test placeShip()", () => {
   let testGameboard;
   beforeEach(() => {
     testGameboard = gameboard();
@@ -47,5 +47,44 @@ describe("test gameboard placeShip()", () => {
     expect(testGameboard.placeShip(4, 2, 7, "horizontal")).toBe(
       "Ship out of map boundaries"
     );
+  });
+});
+
+describe("test receiveAttack()", () => {
+  let testGameboard;
+  beforeEach(() => {
+    testGameboard = gameboard();
+  });
+  it("mark empty cell as hit", () => {
+    expect(testGameboard.getBoard()[6][5][1]).toBe(0);
+    testGameboard.receiveAttack(6, 5);
+    expect(testGameboard.getBoard()[6][5][1]).toBe(1);
+  });
+  it("mark ship cell as hit + ship hitNumber+1", () => {
+    testGameboard.placeShip(4, 6, 8, "vertical");
+    testGameboard.receiveAttack(6, 8);
+    expect(testGameboard.getBoard()[6][8][0].getHitNumber()).toBe(1);
+    expect(testGameboard.getBoard()[7][8][0].getHitNumber()).toBe(1);
+    expect(testGameboard.getBoard()[6][8][1]).toBe(1);
+  });
+  it("Wrong coordinates", () => {
+    expect(testGameboard.receiveAttack(9, 1)).not.toBe("Wrong coordinates");
+    expect(testGameboard.receiveAttack(9, 10)).toBe("Wrong coordinates");
+  });
+  it("are all ships sunk (false)", () => {
+    testGameboard.placeShip(3, 2, 1, "horizontal");
+    testGameboard.placeShip(2, 3, 5, "vertical");
+    expect(testGameboard.areAllShipsSunk()).toBeFalsy();
+  });
+  it("are all ships sunk (true)", () => {
+    testGameboard.placeShip(3, 2, 1, "horizontal");
+    testGameboard.placeShip(2, 3, 5, "vertical");
+    testGameboard.receiveAttack(2, 1);
+    testGameboard.receiveAttack(2, 2);
+    testGameboard.receiveAttack(2, 3);
+
+    testGameboard.receiveAttack(3, 5);
+    testGameboard.receiveAttack(4, 5);
+    expect(testGameboard.areAllShipsSunk()).toBeTruthy();
   });
 });
